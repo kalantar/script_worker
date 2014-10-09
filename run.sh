f7aa3695b49e8656aba9765ce0907414ce215373
Michael Kalantar <kalantar@us.ibm.com>
2014-10-09 17:39:21 -0400

    Include input variable

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 DATE=`date`
 echo "Identified date as $DATE"
 echo $DATE > ${__ARCHIVE__}/date
-echo "{\"date\":\"${DATE}\", \"foo\": \"bar\"}" > $__STATUS__/out
+echo "{\"date\":\"${DATE}\", \"foo\": \"${FOO}\"}" > $__STATUS__/out
